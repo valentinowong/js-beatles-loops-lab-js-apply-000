bdff3fa6e72471c2f527755bc17d1ac63da7fd1a
Valentino Wong <valentino.wong@gmail.com>
2019-04-30 20:54:02 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,9 +10,9 @@ function theBeatlesPlay(musicians, instruments) {
 function johnLennonFacts(facts) {
   var arr = [];
   var i = 0;
-  while i < facts.length {
-    arr.push(`${facts[i]}!!!`)
-    i++
+  while (i < facts.length) {
+    arr.push(`${facts[i]}!!!`);
+    i++;
   }
   return arr;
 }
