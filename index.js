75e52bdb3c64b166764c8bf5bea3df9a8be7c13a
Valentino Wong <valentino.wong@gmail.com>
2019-04-30 20:49:24 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 // add solution here
 function theBeatlesPlay(musicians, instruments) {
-  var arr = []
+  var arr = [];
   for (i = 0; i < musicians.length; i++) {
-    arr.push(`${musicians[i]} plays ${instruments}`)
+    arr.push(`${musicians[i]} plays ${instruments}`);
   }
-  return arr
+  return arr;
 }
